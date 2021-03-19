860435abfb2c0c52142120c1b1561fc8235952be
Vijaykumar Arun Lokhande <vijaysaurabh1999@gmail.com>
2021-03-19 12:32:03 +0530

    whitenoise and procfile

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q865x8o=s_sqkw0pk3t3phsayztq4b=4!n-p!z_o08+c$tf_tz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portfolio-vijaykumaral.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,4 +140,3 @@ try:
     from .local_settings import *
 except ImportError:
     pass
-    
